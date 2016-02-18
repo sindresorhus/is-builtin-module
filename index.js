e4e5c7fcf243298f06ce9bace834410a6f778ee3
thefourtheye <thefourtheye@users.noreply.github.com>
2016-02-18 20:12:25 +0530

    decide based on the resolved path
    as per @vkurchatkin's suggestion, this can be decided without a
    predefined list of module names.

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 'use strict';
-var builtinModules = require('builtin-modules');
 
 module.exports = function (str) {
 	if (typeof str !== 'string') {
 		throw new TypeError('Expected a string');
 	}
 
-	return builtinModules.indexOf(str) !== -1;
+	return require.resolve(str).indexOf(require('path').sep) === -1;
 };

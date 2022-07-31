3ef276d9bbb54621b2bfd4b5ce8ac7f839d94d72
Sindre Sorhus <sindresorhus@gmail.com>
2022-07-31 12:59:07 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,22 @@
 /**
- * Returns `true` if the given `moduleName` is a Node.js builtin module, `false` otherwise.
- *
- * @example
- * ```
- * isBuiltinModule('fs/promises');
- * //=> true
- *
- * isBuiltinModule('node:fs');
- * //=> true
- * 
- * isBuiltinModule('unicorn');
- * //=> false
- * ```
- *
- * @param {string} moduleName Name of the module
- */
+Returns `true` if the given `moduleName` is a Node.js builtin module, `false` otherwise.
+
+@param moduleName  - The name of the module.
+
+@example
+```
+import isBuiltinModule = require('is-builtin-module');
+
+isBuiltinModule('fs/promises');
+//=> true
+
+isBuiltinModule('node:fs');
+//=> true
+
+isBuiltinModule('unicorn');
+//=> false
+```
+*/
 declare function isBuiltinModule(moduleName: string): boolean;
 
 export = isBuiltinModule;
